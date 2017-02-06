da4238161c5101874a3205b1ed3f6abdc137c4c8
khouloudbelguith <khouloud.belguiith@gmail.com>
2017-02-06 15:03:12 +0100

    add greeter class

---
+++ lib/greeter.java
@@ -0,0 +1,13 @@
+
+public Class Greeter()
+{
+private String expression;
+public Greeter(String expression)
+{
+this.expression= expression;
+}
+public String ShowExpression(String expression)
+{
+return expression;
+}
+}
